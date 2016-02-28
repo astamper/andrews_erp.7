5d016ead185651e6e5ee70ae6de1a478f5ddbc5e
Prelang Builder <builder@prelang.com>
2016-02-28 04:31:41 +0000

    Defined validation for attribute 'email' on model 'Order'

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ActiveRecord::Base
   belongs_to :customer
   has_many :order_items
   validates_formatting_of :zip, using: :us_zip
+  validates_formatting_of :email, using: :email
 end
